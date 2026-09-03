0dc0c5060d4fb0b3993a920501db264d91d34b97
luciancelmare <luciancelmare@gmail.com>
2026-09-03 07:13:26 +0000

    add autoremove to APT - images

--- rebuild_kasm-devbox.sh
+++ rebuild_kasm-devbox.sh
@@ -6,3 +6,5 @@ docker build -f ./kasm-images/devbox/dockerfile  -t luciancelmare/kasm-devbox:v3
 
 #docker run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=adm123 luciancelmare/kasm-devbox:v1.1
 docker run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=adm123 luciancelmare/kasm-devbox:v3.1
+
+# docker push luciancelmare/kasm-devbox:v3.1
--- rebuild_kasm-kitty.sh
+++ rebuild_kasm-kitty.sh
@@ -6,3 +6,6 @@ docker build -f ./kasm-images/kitty/dockerfile  -t luciancelmare/kasm-kitty:v1.3
 
 
 docker run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=adm123 luciancelmare/kasm-kitty:v1.3
+
+
+# docker push luciancelmare/kasm-kitty:v1.3
